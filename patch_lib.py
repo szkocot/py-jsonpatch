e9a5e56f1a81292a272b1fdf36b16ed490db0826
Szymon Kocot <szymko1995@gmail.com>
2018-11-29 19:56:00 +0100

    Basic functionality

--- patch_lib.py
+++ patch_lib.py
@@ -3,6 +3,22 @@ class PatchError(Exception):
 
 
 def patch_document(document, commands):
+
+    for command in commands:
+
+        location = command["path"].split("/")[1]
+
+        if command["op"] is "add":
+            document[location] = command["value"]
+        elif command["op"] is "remove":
+            del document[location]
+        elif command["op"] is "replace":
+            document[location] = command["value"]
+        elif command["op"] is "copy":
+            location_from = command["from"].split("/")[1]
+            document[location] = document[location_from]
+        elif command["op"] is "move":
+            location_from = command["from"].split("/")[1]
+            document[location] = document[location_from]
+            del document[location_from]
     return document
-    if "something_is_wrong":
-        raise PatchError('something is wrong, dude')
--- tests/test_patch_lib.py
+++ tests/test_patch_lib.py
@@ -4,3 +4,47 @@ from patch_lib import patch_document
 def test_not_modified_document():
     document = patch_document({"test": "test"}, [])
     assert document == {"test": "test"}
+
+
+def test_add():
+    document = patch_document({"test": "test"}, [
+        {"op": "add",
+         "path": "/test2",
+         "value": "test2"}
+    ])
+    assert document == {"test": "test", "test2": "test2"}
+
+
+def test_remove():
+    document = patch_document({"test": "test"}, [
+        {"op": "remove",
+         "path": "/test"}
+    ])
+    assert document == {}
+
+
+def test_replace():
+    document = patch_document({"test": "test"}, [
+        {"op": "replace",
+         "path": "/test",
+         "value": "test2"}
+    ])
+    assert document == {"test": "test2"}
+
+
+def test_copy():
+    document = patch_document({"test": "test"}, [
+        {"op": "copy",
+         "path": "/test2",
+         "from": "/test"}
+    ])
+    assert document == {"test": "test", "test2": "test"}
+
+
+def test_move():
+    document = patch_document({"test": "test"}, [
+        {"op": "move",
+         "path": "/test2",
+         "from": "/test"}
+    ])
+    assert document == {"test2": "test"}
